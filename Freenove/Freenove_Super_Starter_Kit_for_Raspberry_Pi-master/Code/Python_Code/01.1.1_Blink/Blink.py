bc766c65823bda297d19a4c80506fa5bf347cc36
PrtmPhlp <p.pertermann@icloud.com>
2022-02-04 15:31:07 +0100

    Bugfix

--- Freenove/Freenove_Super_Starter_Kit_for_Raspberry_Pi-master/Code/Python_Code/01.1.1_Blink/Blink.py
+++ Freenove/Freenove_Super_Starter_Kit_for_Raspberry_Pi-master/Code/Python_Code/01.1.1_Blink/Blink.py
@@ -1,38 +1,29 @@
-#!/usr/bin/env python3
 ########################################################################
-# Filename    : Blink.py
-# Description : Basic usage of GPIO. Let led blink.
-# auther      : www.freenove.com
-# modification: 2019/12/28
+# EINSCHALTEN EINER LED VIA GPIO UND PYTHON
 ########################################################################
 import RPi.GPIO as GPIO
-import time
+import time    # Verzögerung
 
-ledPin = 11    # define ledPin
+ledPin = 11    # Benutzter Pin
 
 def setup():
-    GPIO.setmode(GPIO.BOARD)       # use PHYSICAL GPIO Numbering
-    GPIO.setup(ledPin, GPIO.OUT)   # set the ledPin to OUTPUT mode
-    GPIO.output(ledPin, GPIO.LOW)  # make ledPin output LOW level 
-    print ('using pin%d'%ledPin)
+    GPIO.setmode(GPIO.BOARD)       # GPIO "einschalten"
+    GPIO.setup(ledPin, GPIO.OUT)   # LED ausschalt Sequenz "einschalten"
+    GPIO.output(ledPin, GPIO.LOW)  # Die tatsächliche Aktion definieren
 
 def loop():
     while True:
-        GPIO.output(ledPin, GPIO.HIGH)  # make ledPin output HIGH level to turn on led
-        print ('led turned on >>>')     # print information on terminal
-        time.sleep(1)                   # Wait for 1 second
-        GPIO.output(ledPin, GPIO.LOW)   # make ledPin output LOW level to turn off led
-        print ('led turned off <<<')
-        time.sleep(1)                   # Wait for 1 second
+        time.sleep(1)              # 1 Sekunde Verzögerung
+        GPIO.output(ledPin, GPIO.HIGH)   # LED ausschalten
 
 def destroy():
-    GPIO.cleanup()                      # Release all GPIO
+    GPIO.cleanup()                      # Strom aller Pins deaktivieren
 
-if __name__ == '__main__':    # Program entrance
-    print ('Program is starting ... \n')
+if __name__ == '__main__':    # Standart Vorgang zum beenden des Programmes
+    print ('Program is starting ... \n')   # Start Mitteilung
     setup()
     try:
         loop()
-    except KeyboardInterrupt:   # Press ctrl-c to end the program.
+    except KeyboardInterrupt:   # ctrl-c zum stoppen des Programmes
         destroy()
 
