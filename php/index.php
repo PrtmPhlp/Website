c690f5bc89e1a922521ac54c4a332fa83c412d03
PrtmPhlp <p.pertermann@icloud.com>
2022-04-16 16:52:23 +0200

    added favicon

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Login Form</title>
+    </head>
+    <body>
+        
+    <?php
+
+    $people = array("Alice", "Bob", "Catherine");
+
+    foreach ($people as $person) {
+        echo $person . ' ';
+    }
+
+    ?>
+
+    </body>
+</html>
